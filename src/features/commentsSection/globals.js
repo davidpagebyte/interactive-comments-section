856aad98a1b7874a0090e9baf3801af142d15883
David V <david.villavs.dev@gmail.com>
2023-06-29 21:51:54 -0700

    added new componen to select users, and new global module

---
+++ src/features/commentsSection/globals.js
@@ -0,0 +1,30 @@
+const availableUsers = [
+    {
+        'image': {
+            'png': require('./images/avatars/image-juliusomo.png'),
+        },
+        'username': 'juliusomo'
+    },
+    {
+        'image': {
+            'png': require('./images/avatars/image-amyrobson.png'),
+        },
+        'username': 'amyrobson'
+    },
+    {
+        'image': {
+            'png': require('./images/avatars/image-maxblagun.png'),
+        },
+        'username': 'maxblagun'
+    },
+    {
+        'image': {
+            'png': require('./images/avatars/image-ramsesmiron.png'),
+        },
+        'username': 'ramsesmiron'
+    }
+]
+
+export const globalSettings = {
+    availableUsers: availableUsers
+}
